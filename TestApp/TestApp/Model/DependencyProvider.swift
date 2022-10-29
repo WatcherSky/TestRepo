5f22f3fc7bdde653a4d05a20621136c9f384c790
WatcherSky <theworld.com@mail.ru>
2022-10-29 12:55:34 +0600

    commit project

---
+++ TestApp/TestApp/Model/DependencyProvider.swift
@@ -0,0 +1,24 @@
+//
+//  DependencyProvider.swift
+//  TestApp
+//
+//  Created by Владимир on 08.09.2022.
+//
+
+import Foundation
+import UIKit
+
+
+//struct DependencyProvider {
+//    static var service: Service {
+//        return NetworkService()
+//    }
+//    
+//    static var viewController: MainViewController {
+//        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! MainViewController
+//        vc.networkService = service
+//        vc.setupTableView()
+//        return vc
+//    }
+//    
+//}
